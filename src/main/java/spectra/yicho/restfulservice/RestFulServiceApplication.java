a779d70b88b8afdc4df1fef560fe25505d5d21ae
unknown <yicho@spectra.co.kr>
2022-08-29 14:23:43 +0900

    Initial commit

---
+++ src/main/java/spectra/yicho/restfulservice/RestFulServiceApplication.java
@@ -0,0 +1,13 @@
+package spectra.yicho.restfulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFulServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestFulServiceApplication.class, args);
+    }
+
+}
