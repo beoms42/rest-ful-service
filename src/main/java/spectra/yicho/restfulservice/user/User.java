6d41ce29e65363339a0ffe06955741649cca2915
beoms42 <beoms42@gmail.com>
2022-09-01 11:02:03 +0900

    [조영인] Section 4-4 Swagger Customizing

--- src/main/java/spectra/yicho/restfulservice/user/User.java
+++ src/main/java/spectra/yicho/restfulservice/user/User.java
@@ -3,6 +3,8 @@ package spectra.yicho.restfulservice.user;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,16 +18,20 @@ import java.util.Date;
 // @JsonIgnoreProperties(value = {"password"})
 @JsonFilter("UserInfo")
 @NoArgsConstructor
+@ApiModel(description = "사용사 상세 저옵를 위한 도메인 객체")
 public class User {
     private Integer id;
 
     // Validation annotation > 유효성 검사
     @Size(min=2)
+    @ApiModelProperty(notes = "사용자 이름을 입력해 주세요.")
     private String name;
     @Past
+    @ApiModelProperty(notes = "사용자 등록일을 입력해 주세요.")
     private Date joinDate;
-
+    @ApiModelProperty(notes = "패스워드을 입력해 주세요.")
     private String password;
+    @ApiModelProperty(notes = "사용자 주민번호을 입력해 주세요.")
     private String ssn;
 
 }
